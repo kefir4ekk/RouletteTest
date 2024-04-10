8e366ef6f411f15e45170d14bd075607ac4fcb4e
kefir4ekk <112196112+kefir4ekk@users.noreply.github.com>
2024-04-10 21:54:58 +0500

    Commit1

---
+++ RouletteTest.py
@@ -0,0 +1,28 @@
+import random
+
+start_numb = int(input("Enter start number - "))
+end_numb = int(input("Enter end number - "))
+print("Succesfuly")
+
+while True:
+    guess = int(input('Enter Number - '))
+    guess1 = random.randint(start_numb, end_numb)
+
+    if guess == guess1:
+        print("You won!")
+        print("The number was - ", guess1)
+        b = input("Exit? - ")
+        if b == 'Yes':
+            print("Exit")
+            break
+        continue
+
+    else:
+        print("You lose!")
+        print("The number was - ", guess1)
+        print("Try again")
+        b = input("Exit? - ")
+        if b == 'Yes':
+            print("Exit")
+            break
+        continue
